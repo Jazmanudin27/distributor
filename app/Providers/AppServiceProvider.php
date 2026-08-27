9644bc44bea858b4eac9dbedb80a9ba4577041d3
Jazmanudin27 <jazmanudin9@gmail.com>
2026-08-27 11:45:45 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     {
         \Illuminate\Pagination\Paginator::useBootstrapFive();
 
-        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+        if (!in_array(request()->getHost(), ['localhost', '127.0.0.1', '::1'])) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
